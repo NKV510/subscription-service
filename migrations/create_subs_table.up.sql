a978785599d6ad8cec7869c08040bc6f4f599d63
Nikita Karpov <nik-karpov-01@mail.ru>
2025-11-16 19:05:40 +0300

    +

--- migrations/create_subs_table.up.sql
+++ migrations/create_subs_table.up.sql
@@ -1,14 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE subscriptions (
+CREATE TABLE IF NOT EXISTS subscriptions (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     service_name VARCHAR(255) NOT NULL,
     price INTEGER NOT NULL CHECK (price >= 0),
     user_id UUID NOT NULL,
     start_date DATE NOT NULL,
-    end_date DATE NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+    end_date DATE NULL
 );
 
 CREATE INDEX idx_subscriptions_user_id ON subscriptions(user_id);
